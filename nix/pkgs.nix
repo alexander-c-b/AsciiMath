39186aa0b0298c22ee4ac77a5f1a65bfce4106b6
Alexander Bodoh <alexander.c.bodoh@gmail.com>
2020-11-23 17:44:15 -0500

    Use pandoc-types-1.21, Nix+Cabal

---
+++ nix/pkgs.nix
@@ -0,0 +1,1 @@
+import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/14cf906f33c39d38741062df2cca0b0575b210ff.tar.gz")
--- src/bin/Pandoc-AsciiMath.hs
+++ src/bin/Pandoc-AsciiMath.hs
@@ -3,9 +3,10 @@ module Main (main) where
 import Control.Exception as E
 import Text.Pandoc.JSON
 import AsciiMath
+import Data.Text (pack,unpack)
 
 
 main :: IO ()
 main = E.catch (toJSONFilter asciimath) printAndExit
-  where asciimath (Math t s) = Math t (run s)
+  where asciimath (Math t s) = Math t (pack $ run $ unpack s)
         asciimath x = x
