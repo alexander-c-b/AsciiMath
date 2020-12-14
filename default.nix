e6c2798423858a54ec75cb0ce1e504d03fc26e2f
Alexander Bodoh <alexander.c.bodoh@gmail.com>
2020-12-13 22:59:54 -0500

    depend on zandroidius/walk-ast (#14)

--- default.nix
+++ default.nix
@@ -1,3 +1,15 @@
 { pkgs ? import ./nix/pkgs.nix {}, compiler ? "ghc865" }:
 
-pkgs.haskell.packages.${compiler}.callCabal2nix "AsciiMath" ./. {}
+let
+  walk-ast = pkgs.fetchFromGitHub {
+    owner  = "zandroidius";
+    repo   = "walk-ast";
+    rev    = "624fddb77bac1ca3cead3b80cfaec3483ea3e512";
+    sha256 = "095af1fz61mc9qlaa2v3xzkhfbak3s93fmhcv0d9jq5734c9ccwk";
+  };
+  haskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = self: super: { 
+      walk-ast = self.callPackage walk-ast {};
+    };
+  };
+in haskellPackages.callCabal2nix "AsciiMath" ./. {}
