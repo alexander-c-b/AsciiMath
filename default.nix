2e5ead4d5f2b352deccb380dd642c1054b34a7b2
Alexander C. Bodoh <alexander.c.bodoh@gmail.com>
2020-12-23 17:53:17 -0500

    Correct mistake in #17

--- default.nix
+++ default.nix
@@ -1,3 +1,3 @@
 { pkgs ? import ./nix/pkgs.nix {}, compiler ? "ghc865" }:
 
-pkgs.haskellPackages.callCabal2nix "AsciiMath" ./. {}
+pkgs.haskell.packages.${compiler}.callCabal2nix "AsciiMath" ./. {}
